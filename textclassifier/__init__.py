bd0946c07f2701c46144fcb10668e6de50d4bb89
canyon289 <ravinsdrive@gmail.com>
2017-05-29 09:45:43 -0700

    Fixed example so it's running

--- textclassifier/__init__.py
+++ textclassifier/__init__.py
@@ -1,7 +1,2 @@
-import os 
+from .mlcomp_sparse_document_classification import run_analysis
 
-def t_data():
-    t_filepath = os.path.abspath(os.path.join(__file__, '..', 'data', 'test.txt'))
-    f = open(t_filepath)
-    print(f.read())
-    return
