4d3f6a788ad6e9ed07436c572c3953c410f4d10e
canyon289 <ravinsdrive@gmail.com>
2017-06-02 01:35:53 -0700

    Modify feature joining code for Sklearn which turns out works

--- code_tests/sparse_test.py
+++ code_tests/sparse_test.py
@@ -5,6 +5,7 @@ Test to see if I can combine sparse matrices with additional feature engineering
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd 
 import numpy as np
+from scipy.sparse import hstack
 
 train_set = ["This is a positive sentence 7",
             "This is a negative sentence 8"]
@@ -20,8 +21,8 @@ df2 = pd.DataFrame([[10,12],[11,13]], columns = ('a','b'))
 
 c = np.matrix(df["contains7"]).T
 n = np.array([[1,2,3],[4,5,6]])
-np.hstack((n,c))
 
 
 # Test Sparse Stack
-stack = np.hstack((sp,n))
+# Scipy HStack works but npstack doesnt
+stack = hstack((sp,n))
